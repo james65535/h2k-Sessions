9a28013c30645188656ceb8efcd8af8e14935cfc
mcafeej <jmlinked@yahoo.com>
2018-03-12 11:22:33 +1100

    ps1 update

---
+++ Coding/vmCreate.ps1
@@ -0,0 +1,36 @@
+<#
+    Simple vSphere VM Creation Script
+    Written by James McAfee
+    Version: 1.0 - March 12th, 2018
+#>
+
+Get-Module -Name VMware* -ListAvailable | Import-Module
+$vcUser = ""
+$vcPass = ""
+$vcServer = "vcsa-01a.corp.local"
+$vmHost = "esx-01a.corp.local"
+$vmDatastore = "ds-iscsi01"
+
+function createVMs {
+    Param ([int]$numVMs)
+    for ($i = 0; $i -lt $numVMs;$i++) {
+        $vmName = ([char](65+$i))
+        # See `get-help new-vm` and `get-help new-vm -examples` for more info
+        $result = New-VM -Name $vmName -VMHost $vmHost -Datastore $vmDatastore -DiskGB 1 -MemoryGB 1 -NumCpu 1 -Portgroup "VM Network"
+    }
+}
+
+write-host "Starting script"
+$server = Connect-VIserver -server $vcServer -user $vcUser -password $vcPass
+
+$startVMList = Get-VM
+write-host $startVMList
+
+createVMs -numVMs 2
+
+$endVMList = Get-VM
+write-host $endVMList
+
+Disconnect-VIServer -server $server -Confirm:$false
+write-host "Ending script"
+Read-Host -Prompt "Hit key to end"
